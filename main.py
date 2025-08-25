43abdf9e04e280c012150d57bfb2bb7ea144f437
artemisiavelletri-blip <artemisiavelletri@gmail.com>
2025-08-25 14:36:30 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-from fastapi import FastAPI
-from selenium_bot import run_bot
+from fastapi import FastAPI, Query
+from selenium_bot import find_m3u8_url
 
 app = FastAPI()
 
-@app.get("/get_url")
-def scrape():
-    title = run_bot()
-    return {"url": url}
+@app.get("/scrape")
+def scrape(url: str = Query(..., description="L'URL del film da cui estrarre l'm3u8")):
+    m3u8_url = find_m3u8_url(url)
+    if m3u8_url:
+        return {"m3u8_url": m3u8_url}
+    else:
+        return {"error": "404"}
