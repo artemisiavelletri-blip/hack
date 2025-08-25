a156097c5cf249324eb6c0c7bb41c0ddf044098b
artemisiavelletri-blip <artemisiavelletri@gmail.com>
2025-08-25 14:36:48 +0200

    Update selenium_bot.py

--- selenium_bot.py
+++ selenium_bot.py
@@ -6,28 +6,29 @@ import time
 import m3u8
 import requests
 
-# URL del film
-url = "https://vixsrc.to/movie/312221?lang=it"
+def find_m3u8_url(url: str):
+    # Configurazione Chrome headless
+    options = Options()
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
 
-def run_bot():
-  # Configurazione Chrome headless
-  options = Options()
-  options.add_argument("--headless=new")
-  options.add_argument("--no-sandbox")
-  options.add_argument("--disable-dev-shm-usage")
+    # Avvia Chrome con Selenium-Wire
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+    link = ""
 
-  # Avvia Chrome con Selenium-Wire
-  driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-  link = ""
+    try:
+        driver.get(url)
+        print("[*] Caricamento pagina...")
+        time.sleep(8)  # Aspetta che il player carichi
 
-  try:
-      driver.get(url)
-      print("[*] Caricamento pagina...")
-      time.sleep(8) # aspetta che il player carichi
+        print("[*] Cerco URL .m3u8...")
+        for request in driver.requests:
+            if request.response and "https://vixsrc.to/playlist/" in request.url:
+                link = request.url
+                break  # Prendi il primo URL trovato
 
-      print("[*] Cerco URL .m3u8...")
-      for request in driver.requests:
-          if request.response and "https://vixsrc.to/playlist/" in request.url:
-              return request.url
-  finally:
-    driver.quit()
+    finally:
+        driver.quit()
+
+    return link
